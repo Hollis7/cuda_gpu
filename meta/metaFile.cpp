cc7646d52af331957001f314ea40c72e1108e0ab
hollis7 <herryhollis@163.com>
2023-04-28 15:47:34 +0800

    test float equal

--- meta/metaFile.cpp
+++ meta/metaFile.cpp
@@ -2,8 +2,15 @@
 #include <string>
 int main()
 {
-    const char *p = NULL;
-    p = "abc";
-    printf("%s\n", p);
+    double a = 3.1415927f;
+    double b = 3.1415928f;
+    if (a == b)
+    {
+        printf("equal \n");
+    }
+    else
+    {
+        printf("not equal");
+    }
     return 0;
 }
